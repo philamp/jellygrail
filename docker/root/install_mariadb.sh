7ac15547bf6c8a8bf30683a850861cb8d68c422a
philamp <90403351+philamp@users.noreply.github.com>
2024-08-11 14:10:43 +0200

    Update install_mariadb.sh

--- docker/root/install_mariadb.sh
+++ docker/root/install_mariadb.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Update package information and install MariaDB server
-sudo apt update
-sudo apt install -y mariadb-server
+apt update
+apt install -y mariadb-server
 
-sudo sed -i 's/^port\s*=.*/port = 6503/' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i 's/^port\s*=.*/port = 6503/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 # Secure MariaDB installation (automated with the specified answers)
-sudo mysql_secure_installation <<EOF
+mysql_secure_installation <<EOF
 
 y
 n
@@ -18,7 +18,7 @@ y
 EOF
 
 # Log in to MariaDB as root using Unix socket authentication and set up the kodi user
-sudo mysql -u root <<EOF
+mysql -u root <<EOF
 CREATE USER 'kodi'@'%' IDENTIFIED BY 'kodi';
 GRANT ALL PRIVILEGES ON *.* TO 'kodi'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
