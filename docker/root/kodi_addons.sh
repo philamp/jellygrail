afbcb54d0c474ad552de02b9145c61588f0f7b3a
root <phil.gaultier@gmail.com>
2024-08-11 10:57:40 +0000

    kodi addons script rewrite + mv mariadb script

--- docker/root/kodi_addons.sh
+++ docker/root/kodi_addons.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 cd /mounts/kodi/software
-wget https://kodi.jellyfin.org/repository.jellyfin.kodi.zip 
-wget https://github.com/fandangos/JRE-Kodi-Android/releases/download/1.2-aarch64/script.service.jre-aarch64-v1.2.zip 
-wget https://jurialmunkey.github.io/repository.jurialmunkey/repository.jurialmunkey-3.4.zip
-wget https://a4k-openproject.github.io/a4kSubtitles/packages/a4kSubtitles-repository.zip
-wget https://abratchik.github.io/kodi.repository/leia/script.unlock.advancedsettings/script.unlock.advancedsettings-1.0.3.zip
+
+# Define URLs and filenames
+urls=(
+    "https://kodi.jellyfin.org/repository.jellyfin.kodi.zip"
+    "https://github.com/fandangos/JRE-Kodi-Android/releases/download/1.2-aarch64/script.service.jre-aarch64-v1.2.zip"
+    "https://jurialmunkey.github.io/repository.jurialmunkey/repository.jurialmunkey-3.4.zip"
+    "https://a4k-openproject.github.io/a4kSubtitles/packages/a4kSubtitles-repository.zip"
+    "https://abratchik.github.io/kodi.repository/leia/script.unlock.advancedsettings/script.unlock.advancedsettings-1.0.3.zip"
+)
+
+# Loop through URLs
+for url in "${urls[@]}"; do
+    # Extract the filename from the URL
+    filename=$(basename "$url")
+
+    # Check if the file already exists
+    if [ -f "$filename" ]; then
+        echo "$filename already exists, skipping download."
+    else
+        wget "$url"
+    fi
+done
