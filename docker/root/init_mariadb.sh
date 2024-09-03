42318620180b1db9bd12a922f3edb77320bd7e32
philamp <90403351+philamp@users.noreply.github.com>
2024-09-04 01:29:12 +0200

    Update init_mariadb.sh

--- docker/root/init_mariadb.sh
+++ docker/root/init_mariadb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -d /var/lib/mysql/mysql ]; then
   echo "Initializing MariaDB data directory..."
-  mariadb-install-db --user=root --datadir=/var/lib/mysql
+  mariadb-install-db --user=mysql --datadir=/var/lib/mysql
   rm -f /var/lib/mysql/aria_log_control
 fi
