a2e4c342e04ca7de04a4094cb9a7c14b07733fe8
philamp <90403351+philamp@users.noreply.github.com>
2024-09-03 23:00:40 +0200

    Update install_mariadb.sh

--- docker/root/install_mariadb.sh
+++ docker/root/install_mariadb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ ! -f "./mariadb_installed" ]; then
   # Update package information and install MariaDB server
-  apt-get update
-  apt-get install -y mariadb-server
-  sed -i '/^\[mysqld\]/a port = 6503' /etc/mysql/mariadb.conf.d/50-server.cnf
-  sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
-  service mariadb start
+  # apt-get update
+  # DEBIAN_FRONTEND=noninteractive apt-get install -y mariadb-server
+  # sed -i '/^\[mysqld\]/a port = 6503' /etc/mysql/mariadb.conf.d/50-server.cnf
+  # sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+  # service mariadb start
   # Secure MariaDB installation (automated with the specified answers)
   mysql_secure_installation <<EOF
   
@@ -24,7 +24,7 @@ if [ ! -f "./mariadb_installed" ]; then
   FLUSH PRIVILEGES;
   EXIT;
   EOF
-  service mariadb stop
-  echo "MariaDB installation and kodi user setup complete."
+  # service mariadb stop
+  echo "Secure  install and Kodi user setup complete."
   touch ./mariadb_installed
 fi
