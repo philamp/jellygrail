707209048aa2b297bb39e5662819987048d96b0f
philamp <90403351+philamp@users.noreply.github.com>
2024-09-03 22:29:32 +0200

    Update install_mariadb.sh

--- docker/root/install_mariadb.sh
+++ docker/root/install_mariadb.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
-
-# Update package information and install MariaDB server
-apt-get update
-apt-get install -y mariadb-server
-sed -i '/^\[mysqld\]/a port = 6503' /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
-service mariadb start
-# Secure MariaDB installation (automated with the specified answers)
-mysql_secure_installation <<EOF
-
-y
-n
-y
-y
-y
-y
-EOF
-
-# Log in to MariaDB as root using Unix socket authentication and set up the kodi user
-mysql -u root <<EOF
-CREATE USER 'kodi'@'%' IDENTIFIED BY 'kodi';
-GRANT ALL PRIVILEGES ON *.* TO 'kodi'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-EXIT;
-EOF
-service mariadb stop
-echo "MariaDB installation and kodi user setup complete."
+if [ ! -f "./mariadb_installed" ]; then
+  # Update package information and install MariaDB server
+  apt-get update
+  apt-get install -y mariadb-server
+  sed -i '/^\[mysqld\]/a port = 6503' /etc/mysql/mariadb.conf.d/50-server.cnf
+  sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+  service mariadb start
+  # Secure MariaDB installation (automated with the specified answers)
+  mysql_secure_installation <<EOF
+  
+  y
+  n
+  y
+  y
+  y
+  y
+  EOF
+  
+  # Log in to MariaDB as root using Unix socket authentication and set up the kodi user
+  mysql -u root <<EOF
+  CREATE USER 'kodi'@'%' IDENTIFIED BY 'kodi';
+  GRANT ALL PRIVILEGES ON *.* TO 'kodi'@'%' WITH GRANT OPTION;
+  FLUSH PRIVILEGES;
+  EXIT;
+  EOF
+  service mariadb stop
+  echo "MariaDB installation and kodi user setup complete."
+  touch ./mariadb_installed
+fi
