d51498cf445148f27bc8cdf290c724f32f8d3d45
philamp <90403351+philamp@users.noreply.github.com>
2024-09-04 00:05:53 +0200

    Update preinstall_mariadb.sh

--- docker/root/preinstall_mariadb.sh
+++ docker/root/preinstall_mariadb.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+apt-get install -y mariadb-server
+cp /50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
 service mariadb start
 
+# Secure MariaDB installation (automated with the specified answers)
 mysql_secure_installation <<EOF
 
 y
