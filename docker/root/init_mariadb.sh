adf2804a38cf6b99a2c15cddbfa686079ff20e31
philamp <90403351+philamp@users.noreply.github.com>
2024-09-05 19:09:21 +0200

    Delete docker/root/init_mariadb.sh

--- docker/root/init_mariadb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ ! -d /var/lib/mysql/mysql ]; then
-  echo "Initializing MariaDB data directory..."
-  mariadb-install-db --user=mysql --datadir=/var/lib/mysql
-  service mariadb start
-  service mariadb stop
-fi
