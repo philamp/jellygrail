af6177f90b85ad5e917213b4bf0f0cd0c1638c61
philamp <90403351+philamp@users.noreply.github.com>
2025-02-08 18:31:11 +0100

    Update kodi_addons.sh

--- docker/root/kodi_addons.sh
+++ docker/root/kodi_addons.sh
@@ -3,9 +3,6 @@ cd /mounts/kodi/software
 
 # Define URLs and filenames
 urls=(
-    "https://kodi.jellyfin.org/repository.jellyfin.kodi.zip"
-    "https://github.com/fandangos/JRE-Kodi-Android/releases/download/1.2-aarch64/script.service.jre-aarch64-v1.2.zip"
-    "https://jurialmunkey.github.io/repository.jurialmunkey/repository.jurialmunkey-3.4.zip"
     "https://a4k-openproject.github.io/a4kSubtitles/packages/a4kSubtitles-repository.zip"
     "https://abratchik.github.io/kodi.repository/matrix/script.unlock.advancedsettings/script.unlock.advancedsettings-1.0.3.zip"
 )
